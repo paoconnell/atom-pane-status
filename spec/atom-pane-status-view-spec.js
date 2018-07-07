5abd880af402680ca78b914b3b44d624064eaba7
Patrick Alan O'Connell <35573689+paoconnell@users.noreply.github.com>
2018-07-07 19:17:19 +0200

    Bare package created by Atom Package Generator

---
+++ spec/atom-pane-status-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPaneStatusView from '../lib/atom-pane-status-view';
+
+describe('AtomPaneStatusView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
