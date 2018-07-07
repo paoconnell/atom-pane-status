48c8ae0839c0d24303de1467651ee6f194554283
Patrick Alan O'Connell <35573689+paoconnell@users.noreply.github.com>
2018-07-07 19:18:37 +0200

    Show modified status of tile

--- spec/atom-pane-status-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomPaneStatusView from '../lib/atom-pane-status-view';
-
-describe('AtomPaneStatusView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
